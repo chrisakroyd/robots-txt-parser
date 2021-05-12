b952b8ffe5b5030c50e2fd95a7d9e5c414e78efa
Christopher Akroyd <chris@chrisakroyd.com>
2021-05-12 21:11:56 +0100

    Fetch testing.

---
+++ test/robots/fetch.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const robots = require('../../src/index.js');
+
+const expect = chai.expect;
+const robotsParser = robots();
+
+describe('fetch', () => {
+  const links = ['bbc.co.uk', 'guardian.co.uk', 'gov.uk', 'https://www.npmjs.com/', 'www.github.com'];
+
+  it('Expect to be able to retrieve and parse a random selection of websites robots.txt', () => {
+    links.forEach((link) => {
+      robotsParser.fetch(link).then((test) => {
+        expect(Object.keys(test)).to.be.greaterThan(2);
+      });
+    });
+  });
+});
